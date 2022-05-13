eabd6ec054005aafc980c0f2be3cc9ad139b84ff
muradabdullay3v <100687480+muradabdullay3v@users.noreply.github.com>
2022-05-13 19:20:28 +0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(function () {
+        $(document).scroll(function () {
+          var $nav = $(".topnav");
+          $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+      });
+  });
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
